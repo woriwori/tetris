c02816fac3fdc376fedccf63fd09e528cba3343d
이원희 <wonhee_lee@tmax.co.kr>
2020-02-19 17:31:12 +0900

    디자인 틀까지만 완료

--- constants.js
+++ constants.js
@@ -1,1 +1,4 @@
 /* where we put the configurations and rules of the game. */
+const COLS = 10;
+const ROWS = 20;
+const BLOCK_SIZE = 30;
--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 /* code to initialize the game and the overall game logic. */
+const canvas = document.getElementById('board');
+const ctx = canvas.getContext('2d');
+
+// 상수로 canvas 사이즈 계산
+ctx.canvas.width = COLS * BLOCK_SIZE;
+ctx.canvas.height = ROWS * BLOCK_SIZE;
+
+// Scale blocks
+// scalle은 x,y축으로 N배만큼 확대함. (좌표/길이/선두께 포함)
+ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
