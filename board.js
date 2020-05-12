61dcb83f4e1233c3d7e780b98e47051fa6ecaf4e
[3기]이원희 <ireba4478@gmail.com>
2020-05-13 00:54:11 +0900

    board.js, piece.js 코드 분석

--- board.js
+++ board.js
@@ -3,10 +3,13 @@ import Piece from './piece';
 import { COLS, ROWS, BLOCK_SIZE, LINES_PER_LEVEL, KEY, COLORS, POINTS, LEVEL } from './constants';
 import { moves, account, time } from './main';
 export default class Board {
-  ctx;
-  ctxNext;
-  grid;
-  piece;
+  ctx; // 게임 진행 중인 html dom의 canvas 렌더링 컨텍스트
+  ctxNext; // 다음 블럭 보여줄 html dom의 canvas 렌더링 컨텍스트
+  grid; // ROWS * COLS 의 2차원 배열
+  piece; // 현재 블럭(piece)
+  next; // 다음 블럭(piece)
+
+  /* initialize */
   constructor(ctx, ctxNext) {
     this.ctx = ctx;
     this.ctxNext = ctxNext;
@@ -22,11 +25,17 @@ export default class Board {
   }
   reset() {
     // 게임 시작전 보드 초기화
-    // return this.getEmptyBoard();
     this.grid = this.getEmptyGrid();
     this.piece = new Piece(this.ctx);
     this.piece.setStartingPosition();
     this.getNewPiece();
+  }
+
+  getEmptyGrid() {
+    // ROWS * COLS 의 2차원 배열 생성됨.
+    // from 이 {length: N}을 넣으면, 길이가 N인 배열을 생성해주고,
+    // callback의 return 값을 배열의 각 원소로 초기화해줌.
+    return Array.from({ length: ROWS }, () => Array(COLS).fill(0));
   }
 
   getNewPiece() {
@@ -34,12 +43,8 @@ export default class Board {
     this.ctxNext.clearRect(0, 0, this.ctxNext.canvas.width, this.ctxNext.canvas.height);
     this.next.draw();
   }
-  getEmptyGrid() {
-    // ROWS * COLS 의 2차원 배열 생성됨.
-    // from 이 {length: N}을 넣으면, 길이가 N인 배열을 생성해주고,
-    // callback의 return 값을 배열의 각 원소로 초기화해줌.
-    return Array.from({ length: ROWS }, () => Array(COLS).fill(0));
-  }
+
+  /* true: 벽에 안부딪힘 / false: 벽에 부딪힘 */
   valid(p) {
     return p.shape.every((row, dy) => {
       return row.every((value, dx) => {
@@ -58,6 +63,8 @@ export default class Board {
   notOccupied(x, y) {
     return this.grid[y] && this.grid[y][x] === 0;
   }
+
+  /* rotate */
   rotate(piece, direction) {
     // Clone with JSON for immutability.
     // spread 연산자로 하면 1 level 밖에 못하니까 아래처럼 함.
@@ -73,15 +80,19 @@ export default class Board {
     return p;
   }
 
+  /* draw piece & grid */
   draw() {
-    this.piece.draw();
-    this.drawBoard();
+    this.piece.draw(); // ctx 위에 현재 블록을 그림
+    this.drawBoard(); // ctx 위에 현재 블록 전까지 쌓인 블록을 그림
   }
-
   drawBoard() {
+    console.log('----board----');
+    console.table(this.grid);
+    console.log('----board----');
     this.grid.forEach((row, y) => {
       row.forEach((value, x) => {
         if (value > 0) {
+          // freeze() 함수를 통해 grid에 블록이 고정된 경우에만 이 조건실행문에 들어옴.
           this.ctx.fillStyle = COLORS[value];
           this.ctx.fillRect(x, y, 1, 1);
         }
@@ -89,25 +100,36 @@ export default class Board {
     });
   }
 
+  /* 자동으로 떨어지는 동작용 함수 */
+  /*  
+    timer로 계속 떨어지도록 하고 있기 때문에,
+    사용자가 방향키로 조작을 하더라도 계속 이 함수가 호출될 수 밖에 없음. 
+    그래서 사용자가 빠르게 아래키를 눌러서 땅에 닿아도, 
+    이 함수가 호출되기 전까지는 점수가 추가될 수 없음. (즉, 라인이 지워질 수 없음.)
+   */
   drop() {
     let p = moves[KEY.DOWN](this.piece);
     if (this.valid(p)) {
+      // 땅에 안부딪힘
       this.piece.move(p);
     } else {
+      // 땅에 부딪힘
       this.freeze();
       this.clearLines();
       if (this.piece.y === 0) {
+        // 블록이 젤 꼭대기에 닿았을 때
         // Game over
         return false;
       }
-      this.piece = this.next;
-      this.piece.ctx = this.ctx;
+      this.piece = this.next; // 새 블록 추가
+      this.piece.ctx = this.ctx; // 이전 블록이 추가된 grid가 그려진 ctx
       this.piece.setStartingPosition();
-      this.getNewPiece();
+      this.getNewPiece(); // 다음에 나올 블록을 미리 세팅
     }
     return true;
   }
   freeze() {
+    // 블록이 쌓인 경우, 쌓인 블록을 ctx에 고정으로 draw함.
     this.piece.shape.forEach((row, y) => {
       row.forEach((value, x) => {
         if (value > 0) {
@@ -149,7 +171,6 @@ export default class Board {
   }
   getLinesClearedPoints(lines, level) {
     const lineClearPoints = lines === 1 ? POINTS.SINGLE : lines === 2 ? POINTS.DOUBLE : lines === 3 ? POINTS.TRIPLE : lines === 4 ? POINTS.TETRIS : 0;
-
     return (account.level + 1) * lineClearPoints;
   }
 }
--- piece.js
+++ piece.js
@@ -2,8 +2,8 @@ import { COLORS, SHAPES } from './constants';
 /* for piece logic. */
 export default class Piece {
   /* 
-  x : board에서의 x 좌표
-  y : board에서의 y 좌표
+  x : grid에서의 x 좌표
+  y : grid에서의 y 좌표
   color : block 색
   shape : block 을 행렬로 표현한 2차원 배열
   ctx : canvas 객체
@@ -22,17 +22,19 @@ export default class Piece {
     this.typeId = this.randomizeTetrominoType(COLORS.length - 1);
     this.shape = SHAPES[this.typeId];
     this.color = COLORS[this.typeId];
-    // starting position
-    this.x = 3;
+
+    // set starting position
+    this.setStartingPosition();
     this.y = 0;
   }
   draw() {
     this.ctx.fillStyle = this.color;
     this.shape.forEach((row, y) => {
       row.forEach((value, x) => {
-        // x, y 는 shape 내에서 블록의 위치
-        // this.x + x 는 보드상 블록 위치
+        // x, y: shape 2차원 배열내에서 1의 값을 가진 원소의 인덱스
+        // this.x, this.y : grid 내의 블록 위치
         if (value > 0) {
+          // 원소 값이 1이면, 색을 채워서 블록 모양이 표시되도록함. (SHAPES 상수 참조)
           this.ctx.fillRect(this.x + x, this.y + y, 1, 1);
         }
       });
